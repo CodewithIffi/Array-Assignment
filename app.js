a4b87e1356344a888e7ef7f79562e9e2dd3decbd
unknown <ifficodder@gmail.com>
2024-03-12 08:58:14 +0500

    Q 1 to Q 4 have been solved

---
+++ app.js
@@ -0,0 +1,41 @@
+// Q no: 01 Declare an empty array using JS literal notation to store
+// student names in future.
+
+// Answer!
+// let futureStudentName = [ ];
+// console.log(futureStudentName);
+
+
+
+// Q no: 02 Declare an empty array using JS object notation to store
+// student names in future.
+
+// Answer!
+// let futureStudentName = {};
+// futureStudentName['student1'] = 'Irfan';
+// console.log(futureStudentName ['student1']);
+
+
+// Q no: 03 Declare and initialize a strings array
+
+
+// Answer!
+// let stringsArray = ['string1', 'string2', 'string3'];
+
+// stringsArray.push("string1");
+// stringsArray.push("string2");
+// stringsArray.push("string3");
+//  console.log(stringsArray);
+
+
+// Q n0: 04 Declare and initialize a numbers array.
+
+
+// Answer!
+let numbersArray = [1, 2, 3, 4, 5];
+numbersArray.push(1);
+numbersArray.push(2);
+numbersArray.push(3);
+numbersArray.push(4);
+numbersArray.push(5);
+console.log(numbersArray);
